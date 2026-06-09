829d4c3f4feb1118d33dc65e529a393dc7d47ff8
Ali <159713281+cunkin375@users.noreply.github.com>
2026-06-09 18:53:47 -0400

    chore: install react-three-fiber and rei deps

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   // This creates a self-contained build that avoids needing the full app node_modules at runtime
   // (Next.js bundles a minimal node_modules inside the standalone output)
   output: "standalone",
+  // Because we're using Next.js, this is needed to prevent untranspiled addons in the three.js ecosystem
+  // A transpiled package is converted from specialized syntax (Ex. TypeScript) into standard javascript
+  // This makes it compatible with most/older browser environments and skips extra build steps
+  transpilePackages: ["three"],
 };
 
 export default nextConfig;
