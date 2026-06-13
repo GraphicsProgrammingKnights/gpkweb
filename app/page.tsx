de7147c0388b913c4e5fe671f1f8e19018108012
Sebastian Noel <snoel.dev@gmail.com>
2026-06-12 20:16:22 -0400

    refactor/page-section-scaffhold

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,29 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-text-primary">
-      
-
-    {/*START OF THE HOME SECTION */}
-      <section 
-        id="home"
-        className="flex min-h-screen flex-col items-center justify-between p-24 text-text-primary"
-      >
-        {/*Home code goes here */}
-      <h1>Graphics Programming Knights (Home)</h1>
+      {/* START OF THE HOME SECTION */}
+      <section id="home" className="min-h-screen w-full flex flex-col items-center">
+        {/* Home code goes here */}
+        <h2>Home</h2>
       </section>
 
+      {/* START OF THE ABOUT SECTION */}
+      <section id="about" className="min-h-screen w-full flex flex-col items-center">
+        {/* About code goes here */}
+        <h2>About</h2>
+      </section>
 
-    {/*START OF THE ABOUT SECTION */}
-    <section
-    id="about"
-    className="flex min-h-screen flex-col items-center justify-between p-24 text-text-primary"
-    >
-      {/*About code goes here */}
-      <h1>About</h1>
-    </section>
+      {/* START OF THE CHALLENGES SECTION */}
+      <section id="challenges" className="min-h-screen w-full flex flex-col items-center">
+        {/* Challenges code goes here */}
+        <h2>Challenges</h2>
+      </section>
 
-    {/*START OF THE CHALLENGES SECTION*/}
-    <section
-    id="challenges"
-    className="flex min-h-screen flex-col items-center justify-between p-24 text-text-primary"
-    >
-      {/*Challenges code goes here */}
-      <h1>Challenges</h1>
-    </section>
-
-    {/*START OF THE TEAM SECTION*/}
-    <section
-    id="team"
-    className="flex min-h-screen flex-col items-center justify-between p-24 text-text-primary"
-    >
-      {/*Team code goes here */}
-      <h1>Team</h1>
-    </section>
+      {/* START OF THE TEAM SECTION */}
+      <section id="team" className="min-h-screen w-full flex flex-col items-center">
+        {/* Team code goes here */}
+        <h2>Team</h2>
+      </section>
     </main>
   );
 }
