9825666730b0322940e42ee2d582a3dceb924b6f
nicky <nickyalejb@gmail.com>
2026-06-15 02:36:35 -0400

    feat: built navbar component with active section tracking

---
+++ components/Navbar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const sections = ["home", "about", "challenges", "team"];
+
+export default function Navbar() {
+  const [activeSection, setActiveSection] = useState("home");
+
+  /* views each section and updates when 50% visible in viewport yuhh */
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id);
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    /* loops thru each array and if it exist, it will observe section for updates */
+    sections.forEach((id) => {
+      const element = document.getElementById(id);
+
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    /* if navbar component ever gets removed, this stops wasting resources in the background (in case)*/
+    return () => observer.disconnect();
+  }, []);
+ 
+  /* contains navbar w gpk tiny logo on left and blurs anything near navbar*/
+  return (
+    <nav className="sticky top-0 z-50 bg-page-background backdrop-blur-sm">
+      <div className="flex items-center justify-between px-6 py-4">
+        <div>
+          <a href="#home" className="font-bold text-text-primary">
+            GPK
+          </a>
+        </div>
+
+        {/* active section gets filled white box!!! */}
+        <div className="flex gap-8">
+          {sections.map((section) => (
+            <a
+              key={section}
+              href={`#${section}`}
+              className={`capitalize ${
+                activeSection === section
+                  ? "rounded-md border border-text-primary  bg-white text-black px-3 py-1"
+                  : "border border-transparent rounded-md text-text-primary px-3 py-1"
+              }`}
+            >
+              {section}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+}
